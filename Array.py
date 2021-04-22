406e2d653d2ef2ee6266cbf4e9cf349d096353a7
Nathan <58734424+Nathiington@users.noreply.github.com>
2021-04-22 16:01:10 +0200

    After lab 5

---
+++ Array.py
@@ -0,0 +1,7 @@
+def university(departments):
+    for x in departments:
+        print(x)
+
+
+fos = ["CSIS", "EARTH!", "Maths", "Chem", "Bio"]
+university(fos)
---
+++ sum.py
@@ -0,0 +1,9 @@
+# sum of 2 numbers using a function
+def sumfunction(x, y):
+    return x + y
+
+
+a = input('Enter first number: ')
+b = input('Enter second number: ')
+
+print(sumfunction(a, b))
